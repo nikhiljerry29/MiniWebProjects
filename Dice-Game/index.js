169bd95af0062b9ab3f5dc3af4645cbe57bc619c
NIKHIL GUPTA <nikhil.gupta5667@gmail.com>
2021-01-09 21:19:12 +0530

    Dice Game Update

---
+++ Dice-Game/index.js
@@ -0,0 +1,15 @@
+let playerOneDiceValue = Math.floor(Math.random() * 6) + 1;
+let playerOneImagePath = "/images/dice" + playerOneDiceValue + ".png";
+document.querySelector("div img.img1").setAttribute("src", playerOneImagePath)
+
+let playerTwoDiceValue = Math.floor(Math.random() * 6) + 1;
+let playerTwoImagePath = "/images/dice" + playerTwoDiceValue + ".png";
+document.querySelector("div img.img2").setAttribute("src", playerTwoImagePath)
+
+let resultHeading = document.querySelector("h1")
+if(playerOneDiceValue > playerTwoDiceValue)
+	resultHeading.innerText = "Player 1 Wins!!"
+else if (playerOneDiceValue < playerTwoDiceValue)
+	resultHeading.innerText = "Player 2 Wins!!"
+else
+	resultHeading.innerText = "Draw!!"
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # MiniWebProjects
+Here yopu can find my mini web projects.
+
+1. [Dice Game](Dice-Game)
